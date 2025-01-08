07ffb13c66c813280890fb0e4d1d69af0c998a74
pinder99 <pinder.chaudhary@gmail.com>
2025-01-08 18:44:42 +0530

    test

---
+++ javascripts/discourse/components/custom-homepage2-content.js
@@ -0,0 +1,12 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
+
+export default class CustomHomepageContent extends Component {
+  @service router;
+
+  get isHomepage() {
+    const { currentRouteName } = this.router;
+    return currentRouteName === `discovery.${defaultHomepage()}`;
+  }
+}
