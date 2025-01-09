35c495a56fe3e51d24008f0e2a8f4d4431d06284
pinder99 <pinder.chaudhary@gmail.com>
2025-01-09 12:11:18 +0530

    new

--- javascripts/discourse/components/custom-homepage3-content.js
+++ javascripts/discourse/components/custom-homepage3-content.js
@@ -1,12 +1,14 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class CustomHomepageContent extends Component {
   @service router;
 
-  get isHomepage() {
-    const { currentRouteName } = this.router;
-    return currentRouteName === `discovery.${defaultHomepage()}`;
+  get isCustomHomepage() {
+    const { currentURL } = this.router;
+    const customPaths = ['/c/rescources/9', '/c/rescources/9'];
+    
+    console.log("Current URL:", currentURL);  // Debugging: Check the URL
+    return customPaths.includes(currentURL);
   }
 }
